4eea0dfa54689d366fcd41c2f67077e374883f68
jpkhawam <jeanpaulkhawam2001@gmail.com>
2022-02-02 23:00:22 +0200

    renamed files

--- src/main/java/com/example/javafxwordle/MainController.java
+++ src/main/java/com/example/javafxwordle/MainController.java
@@ -3,7 +3,7 @@ package com.example.javafxwordle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
