4749a7a93eb209b46a7b35a90140780b858633ad
Jean-Paul Khawam <jeanpaulkhawam2001@gmail.com>
2022-03-09 15:11:04 +0200

    added icon for window

--- src/main/java/com/example/javafxwordle/MainApplication.java
+++ src/main/java/com/example/javafxwordle/MainApplication.java
@@ -43,6 +43,7 @@ public class MainApplication extends Application {
         stage.setMaxWidth(screenWidth);
         stage.setMaxHeight(screenHeight);
         stage.setTitle("WordleFX");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/icon.png"))));
         stage.setScene(scene);
         stage.show();
 
